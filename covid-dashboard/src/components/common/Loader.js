f338d6ae075d00bfa8608a32f57d4170eda0b8be
edogola4 <edogola4@gmail.com>
2025-04-07 18:39:26 +0300

    updated

--- covid-dashboard/src/components/common/Loader.js
+++ covid-dashboard/src/components/common/Loader.js
@@ -1,8 +1,19 @@
 // src / components / common / Loader.js
 import React from 'react';
+import '../../styles/components/charts.css';
 
-const Loader = () => {
-  return <div className="loader">Loading...</div>;
+const Loader = ({ size = 'medium', text = 'Loading data...' }) => {
+  return (
+    <div className={`loader-container size-${size}`}>
+      <div className="loader-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      {text && <p className="loader-text">{text}</p>}
+    </div>
+  );
 };
 
 export default Loader;
