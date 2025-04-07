ca7e97139492d00b43f5f3d2e010e30c9e6a0c8e
edogola4 <edogola4@gmail.com>
2025-04-07 14:44:57 +0300

    Redux store setup

---
+++ covid-dashboard/src/redux/actions/uiActions.js
@@ -0,0 +1,81 @@
+// src/redux/actions/uiActions.js
+import {
+    toggleTheme,
+    setSidebarOpen,
+    setSidebarWidth,
+    setGridLayout,
+    setChartSize,
+    addNotification,
+    removeNotification,
+    setLoading,
+    setError,
+    clearError,
+  } from '../reducers/uiReducer';
+  
+  // Toggle between light and dark theme
+  export const toggleThemeAction = () => (dispatch) => {
+    dispatch(toggleTheme());
+  };
+  
+  // Control sidebar state
+  export const toggleSidebar = (isOpen) => (dispatch) => {
+    dispatch(setSidebarOpen(isOpen));
+  };
+  
+  export const changeSidebarWidth = (width) => (dispatch) => {
+    dispatch(setSidebarWidth(width));
+  };
+  
+  // Layout customization
+  export const changeGridLayout = (layout) => (dispatch) => {
+    dispatch(setGridLayout(layout));
+  };
+  
+  export const changeChartSize = (size) => (dispatch) => {
+    dispatch(setChartSize(size));
+  };
+  
+  // Notification system
+  export const showNotification = (notification) => (dispatch) => {
+    const { message, type = 'info', duration = 5000 } = notification;
+    
+    const notificationObj = {
+      message,
+      type,
+      duration,
+    };
+    
+    dispatch(addNotification(notificationObj));
+    
+    // Auto-remove notification after duration
+    if (duration) {
+      setTimeout(() => {
+        dispatch(removeNotification(notificationObj.id));
+      }, duration);
+    }
+  };
+  
+  export const dismissNotification = (id) => (dispatch) => {
+    dispatch(removeNotification(id));
+  };
+  
+  // Loading states
+  export const setComponentLoading = (component, isLoading) => (dispatch) => {
+    dispatch(setLoading({ component, isLoading }));
+  };
+  
+  // Error handling
+  export const setGlobalError = (error) => (dispatch) => {
+    dispatch(setError(error));
+    
+    // Also show as notification
+    dispatch(showNotification({
+      message: error,
+      type: 'error',
+      duration: 10000, // Show errors for longer
+    }));
+  };
+  
+  export const clearGlobalError = () => (dispatch) => {
+    dispatch(clearError());
+  };
