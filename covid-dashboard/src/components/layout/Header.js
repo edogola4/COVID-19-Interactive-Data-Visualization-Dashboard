1154ef32479a7a8b13ed1e9c18e11ca771ab8697
edogola4 <edogola4@gmail.com>
2025-04-07 16:54:00 +0300

    created layout header

---
+++ covid-dashboard/src/components/layout/Header.js
@@ -0,0 +1,12 @@
+// src / compnents / layout / Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>COVID-19 Dashboard</h1>
+    </header>
+  );
+};
+
+export default Header;
