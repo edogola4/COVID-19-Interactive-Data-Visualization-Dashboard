15e92b2dd08a99d158d2d7f531bd3d57ea1bf665
edogola4 <edogola4@gmail.com>
2025-04-07 17:16:11 +0300

    created chart config

---
+++ covid-dashboard/src/constants/chartConfigs.js
@@ -0,0 +1,18 @@
+// src / constants / chartConfigs.js
+
+export const CHART_CONFIGS = {
+    BAR_CHART: {
+      width: 800,
+      height: 400,
+      margin: { top: 20, right: 30, bottom: 50, left: 60 },
+    },
+    LINE_CHART: {
+      width: 800,
+      height: 400,
+      margin: { top: 20, right: 30, bottom: 50, left: 60 },
+    },
+    PIE_CHART: {
+      width: 400,
+      height: 400,
+    },
+  };
