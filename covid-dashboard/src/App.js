7fbafecdeb111ce8bbbca5f6d8ec8790bf270638
edogola4 <edogola4@gmail.com>
2025-04-07 17:59:07 +0300

    updated

--- covid-dashboard/src/App.js
+++ covid-dashboard/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Dashboard from './components/dashboard/Dashboard';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import useFetch from './hooks/useFetch';
+import './styles/global.css';
 
-function App() {
+const AppContent = () => {
+  useFetch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <Footer />
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+};
 
 export default App;
