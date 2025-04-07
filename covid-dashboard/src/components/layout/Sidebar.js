be238fe4d99b064207f719165fcdb1a3de9d7bc3
edogola4 <edogola4@gmail.com>
2025-04-07 17:10:44 +0300

    created sidebar

---
+++ covid-dashboard/src/components/layout/Sidebar.js
@@ -0,0 +1,13 @@
+// src / components / layout / Sidebar.js
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <h2>Filters</h2>
+      {/* Additional filters can be added here */}
+    </aside>
+  );
+};
+
+export default Sidebar;
